fc97eaaf1456413bd2bfb4b4bca3ad61611b3b65
dao_mingze <dao_mingze@163.com>
2023-03-16 23:38:16 +0800

    update for debug

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # zhukebot
 
+[![pdm-managed](https://img.shields.io/badge/python-3.9+-blue)](https://pdm.fming.dev)
+[![pdm-managed](https://img.shields.io/badge/pdm-managed-blueviolet)](https://pdm.fming.dev)
 ![licese](https://img.shields.io/github/license/DaoMingze/zhukebot)
 ![GitHub repo size](https://img.shields.io/github/repo-size/daomingze/zhukebot)
 <!--![GitHub release](https://img.shields.io/github/v/release/daomingze/zhukebot)-->
@@ -7,7 +9,10 @@
 
 烛客：我的nonebot机器人
 
+运行环境：debian11、cuda11.8、Python3.9.2、pythorch2.0、nonebot2
+
 插件来源
 
 - sdwebui，精简自[nonebot-plugin-novelai](https://github.com/sena-nana/nonebot-plugin-novelai)，![licese](https://img.shields.io/github/license/sena-nana/nonebot-plugin-novelai?style=flat-square)
-- arkcord，来自[nonebot_plugin_arkrecord](https://github.com/zheuziihau/nonebot_plugin_arkrecord)，![licese](https://img.shields.io/github/license/zheuziihau/nonebot_plugin_arkrecord?style=flat-square)，更改数据库位置
+- arkcord，来自[nonebot_plugin_arkrecord](https://github.com/zheuziihau/nonebot_plugin_arkrecord)，![licese](https://img.shields.io/github/license/zheuziihau/nonebot_plugin_arkrecord?style=flat-square)，仅更改资源位置等。
+- chatGLM，为清华开源的chatGLM-6B提供nonebot插件
--- bot.py
+++ bot.py
@@ -1,35 +1,38 @@
 # -*- coding: utf-8 -*-
-import nonebot
 import os
+
+import nonebot
 from nonebot.adapters.onebot.v11 import Adapter
 
 right_path = __file__.rstrip(os.path.basename(__file__))  # 获取当前文件的所在路径
 os.chdir(right_path)  # 将工作路径改至目标路径
 
 # Custom your logger
-#
-# from nonebot.log import logger, default_format
-# logger.add("error.log",
-#            rotation="00:00",
-#            diagnose=False,
-#            level="ERROR",
-#            format=default_format)
+from nonebot.log import logger, default_format
 
+logger.add(
+    "error.log",
+    rotation="00:00",
+    retention="1 week",
+    diagnose=False,
+    level="ERROR",
+    format=default_format,
+    encoding="utf-8",
+)
 # You can pass some keyword args config to init function
 nonebot.init()
-app = nonebot.get_asgi()
+# Please DO NOT modify this file unless you know what you are doing!
 driver = nonebot.get_driver()
 driver.register_adapter(Adapter)
-# Please DO NOT modify this file unless you know what you are doing!
 # As an alternative, you should use command `nb` or modify `pyproject.toml` to load plugins
+# nonebot.load_builtin_plugin("echo")
+# nonebot.load_plugin("nonebot_plugin_gocqhttp")
+# nonebot.load_plugins("zhukebot/plugins")
 nonebot.load_from_toml("pyproject.toml")
-
 # Modify some config / config depends on loaded configs
-#
 # config = driver.config
 # do something...
-
-
+app = nonebot.get_asgi()
 if __name__ == "__main__":
     nonebot.logger.warning(
         "Always use `nb run` to start the bot instead of manually running!"
